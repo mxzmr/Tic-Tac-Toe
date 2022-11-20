03392e82252ed254b438ae51eebb2607a4e4f15b
mxzmr <maxzam88@gmail.com>
2022-11-20 13:30:17 +0200

    Fixed setTimeOut bug and add css effects

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
 /* eslint-disable prefer-destructuring */
+// there is an issue with score() function its getting called many times
+
 
 const gameBoard = (() => {
-  const container = document.querySelector('.container');
-  const positionOne = document.querySelector('.one');
-  const positionTwo = document.querySelector('.two');
-  const positionThree = document.querySelector('.three');
-  const positionFour = document.querySelector('.four');
-  const positionFive = document.querySelector('.five');
-  const positionSix = document.querySelector('.six');
-  const positionSeven = document.querySelector('.seven');
-  const positionEight = document.querySelector('.eight');
-  const positionNine = document.querySelector('.nine');
   const gameBoard = [];
   const playerTurn = 'x';
+  let playerX = 0;
+  let playerO = 0;
+  let tie = 0;
   return {
     gameBoard,
     playerTurn,
-    container,
-    positionOne,
-    positionTwo,
-    positionThree,
-    positionFour,
-    positionFive,
-    positionSix,
-    positionSeven,
-    positionEight,
-    positionNine,
+    playerX,
+    playerO,
+    tie,
   };
 })();
 
-const players = (() => {
+const display = () => {
+  document.querySelector('.one').textContent = gameBoard.gameBoard[0];
+  document.querySelector('.two').textContent = gameBoard.gameBoard[1];
+  document.querySelector('.three').textContent = gameBoard.gameBoard[2];
+  document.querySelector('.four').textContent = gameBoard.gameBoard[3];
+  document.querySelector('.five').textContent = gameBoard.gameBoard[4];
+  document.querySelector('.six').textContent = gameBoard.gameBoard[5];
+  document.querySelector('.seven').textContent = gameBoard.gameBoard[6];
+  document.querySelector('.eight').textContent = gameBoard.gameBoard[7];
+  document.querySelector('.nine').textContent = gameBoard.gameBoard[8];
+};
+
+const playersInfo = (() => {
   const form = document.querySelector('.start-form');
   const formModal = document.querySelector('.form-modal');
-  const startBtn = document.querySelector('.start-button');
   const submitBtn = document.querySelector('.submit-button');
   const playerOne = document.getElementById('player-one');
   const playerTwo = document.getElementById('player-two');
@@ -41,7 +40,7 @@ const players = (() => {
     if (twoPlayers.value === 'players' && gameBoard.gameBoard.length === 0) {
       formModal.showModal();
       formModal.style.display = 'grid';
-    } else if (document.querySelector('select').selectedIndex === 0){
+    } else if (document.querySelector('select').selectedIndex === 0) {
       ai();
     }
   });
@@ -53,215 +52,146 @@ const players = (() => {
     event.preventDefault();
     const player1 = playerOne.value;
     const player2 = playerTwo.value;
-    if (player1 && player2) {
+    if (player1 && player2 && document.querySelector('.players').selectedIndex === 1) {
       document.querySelector('.players-names').textContent = `${player1} (X) vs ${player2} (O)`;
+      document.querySelector('.score-player').textContent = `${player1} (X) : 0`;
+      document.querySelector('.score-ai').textContent = `${player2} (O) : 0`;
     }
   });
   return { playerOne, playerTwo };
 })();
 
-const display = () => {
-  gameBoard.positionOne.textContent = gameBoard.gameBoard[0];
-  gameBoard.positionTwo.textContent = gameBoard.gameBoard[1];
-  gameBoard.positionThree.textContent = gameBoard.gameBoard[2];
-  gameBoard.positionFour.textContent = gameBoard.gameBoard[3];
-  gameBoard.positionFive.textContent = gameBoard.gameBoard[4];
-  gameBoard.positionSix.textContent = gameBoard.gameBoard[5];
-  gameBoard.positionSeven.textContent = gameBoard.gameBoard[6];
-  gameBoard.positionEight.textContent = gameBoard.gameBoard[7];
-  gameBoard.positionNine.textContent = gameBoard.gameBoard[8];
-};
 const score = () => {
   const modal = document.querySelector('.modal');
+  const modalPara = document.querySelector('.modal-p');
+  const modalPlayers = document.querySelector('.modal-player');
+  const reset = document.querySelector('.reset');
+  const scorePlayer = document.querySelector('.score-player');
+  const scoreAi = document.querySelector('.score-ai');
+  const tie = document.querySelector('.tie');
   const x = [];
   const o = [];
-  let counter = 0;
-  const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+  const winCombo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
     [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], ['na']];
-  for (let i = 0; i < gameBoard.gameBoard.length; i++) {
-    if (gameBoard.gameBoard[i] === 'x') {
-      x.push(i);
-    } else if (gameBoard.gameBoard[i] === 'o') {
-      o.push(i);
-    }
-  }
-  for (let i = 0; i < gameBoard.gameBoard.length; i++) {
-    if (!players.playerOne.value) {
-      players.playerOne.value = 'X';
-    }
-    if (!players.playerTwo.value) {
-      players.playerTwo.value = 'O';
-    }
-    if (win[i].every((ele) => x.includes(ele)) && (!document.querySelector('.modal').open)) {
-      document.querySelector('.modal-player').textContent = `${players.playerOne.value}`;
-      document.querySelector('.modal-p').textContent = 'Won!';
-      document.querySelector('.modal').showModal();
-      document.querySelector('.modal').style.display = 'flex';
-    } else if (win[i].every((ele) => o.includes(ele))) {
-      document.querySelector('.modal-player').textContent = `${players.playerTwo.value}`;
-      document.querySelector('.modal-p').textContent = 'Won!';
-      document.querySelector('.modal').showModal();
-      document.querySelector('.modal').style.display = 'flex';
-    } else if (gameBoard.gameBoard[i]) {
-      counter += 1;
-      if (counter === 9) {
-        document.querySelector('.modal-player').textContent = `${players.playerOne.value}${players.playerTwo.value}`;
-        document.querySelector('.modal-p').textContent = 'Tie!';
-        document.querySelector('.modal').showModal();
-        document.querySelector('.modal').style.display = 'flex';
+  const choiceCombo = (() => {
+    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+      if (gameBoard.gameBoard[i] === 'x') {
+        x.push(i);
+      } else if (gameBoard.gameBoard[i] === 'o') {
+        o.push(i);
       }
     }
-  }
-  document.querySelector('.reset').addEventListener('click', () => {
+  })();
+  const game = (() => {
+    if (!playersInfo.playerOne.value) {
+      playersInfo.playerOne.value = 'Player';
+    }
+    if (!playersInfo.playerTwo.value) {
+      playersInfo.playerTwo.value = 'Player';
+    }
+    let tieCounter = 0;
+    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+      const winnerAnimation = () => {
+        document.getElementById(`${winCombo[i][0]}`).classList.add('blink');
+        document.getElementById(`${winCombo[i][1]}`).classList.add('blink');
+        document.getElementById(`${winCombo[i][2]}`).classList.add('blink');
+        setTimeout(() => {
+          document.getElementById(`${winCombo[i][0]}`).classList.remove('blink');
+          document.getElementById(`${winCombo[i][1]}`).classList.remove('blink');
+          document.getElementById(`${winCombo[i][2]}`).classList.remove('blink');
+          gameBoard.gameBoard = [];
+        }, 1000);
+      };
+      if (winCombo[i].every((ele) => x.includes(ele))) {
+        gameBoard.playerX += 1;
+        gameBoard.playerTurn = 'x';
+        scorePlayer.textContent = `${playersInfo.playerOne.value} (X) : ${gameBoard.playerX}`;
+        winnerAnimation();
+      } else if (winCombo[i].every((ele) => o.includes(ele))) {
+        gameBoard.playerO += 1;
+        gameBoard.playerTurn = 'x';
+        scoreAi.textContent = `${playersInfo.playerTwo.value} (O) : ${gameBoard.playerO}`;
+        winnerAnimation();
+      } else if (gameBoard.gameBoard[i]) {
+        tieCounter += 1;
+        if (tieCounter === 9) {
+          gameBoard.tie += 1;
+          tie.textContent = `Tie : ${gameBoard.tie}`;
+          gameBoard.playerTurn = 'x';
+          gameBoard.gameBoard = [];
+        }
+      }
+    }
+    setTimeout(() => {
+      display();
+    }, 1000);
+    return tieCounter;
+  })();
+
+  const winner = (() => {
+    if (gameBoard.playerX === 3 && (!document.querySelector('.modal').open)) {
+      modalPlayers.textContent = `${playersInfo.playerOne.value} (X)`;
+      modalPara.textContent = 'Won!';
+      modal.showModal();
+      modal.style.display = 'flex';
+    }
+    if (gameBoard.playerO === 3 && (!document.querySelector('.modal').open)) {
+      modalPlayers.textContent = `${playersInfo.playerTwo.value} (O)`;
+      modalPara.textContent = 'Won!';
+      modal.showModal();
+      modal.style.display = 'flex';
+    }
+    if (gameBoard.tie === 3 && (!document.querySelector('.modal').open)) {
+      modalPlayers.textContent = `${playersInfo.playerOne.value}${playersInfo.playerTwo.value}`;
+      modalPara.textContent = 'Tie!';
+      modal.showModal();
+      modal.style.display = 'flex';
+    }
+  })();
+  reset.addEventListener('click', () => {
     document.querySelector('.modal').close();
     gameBoard.gameBoard = [];
     gameBoard.playerTurn = 'x';
-    players.playerOne.value = '';
-    players.playerTwo.value = '';
-    document.querySelector('.modal-player').textContent = '';
+    playersInfo.playerOne.value = '';
+    playersInfo.playerTwo.value = '';
+    modalPlayers.textContent = '';
     document.querySelector('.players-names').textContent = 'Player (X) vs player (O)';
+    modal.style.display = 'none';
     document.querySelector('select').selectedIndex = 0;
-    document.querySelector('.modal').style.display = 'none';
+    scorePlayer.textContent = 'Player (X) : 0';
+    scoreAi.textContent = 'A.I (O) : 0';
+    tie.textContent = 'Tie : 0';
+    gameBoard.playerX = 0;
+    gameBoard.playerO = 0;
+    gameBoard.tie = 0;
     display();
   });
+  return { game, x, winCombo };
 };
 const ai = () => {
   if (document.querySelector('.players').selectedIndex === 0) {
     const aiChoice = Math.round(Math.random() * 8);
-    if (gameBoard.playerTurn === 'o' && (!gameBoard.gameBoard[aiChoice]) && (!document.querySelector('.modal').open)) {
+    if (gameBoard.playerTurn === 'o' && (!gameBoard.gameBoard[aiChoice])) {
       gameBoard.gameBoard[aiChoice] = 'o';
       gameBoard.playerTurn = 'x';
       setTimeout(() => {
-        score();
-      }, 300);
-    } else if (gameBoard.playerTurn === 'o' && (!document.querySelector('.modal').open)) {
+        display();
+      }, 500);
+      score();
+    } else if (gameBoard.playerTurn === 'o') {
       ai();
     }
-    setTimeout(() => {
-      display();
-    }, 250);
   }
 };
 const playerChoices = (() => {
-  gameBoard.positionOne.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[0]) {
+  const container = document.querySelector('.container');
+  container.addEventListener('click', (e) => {
+    if (!gameBoard.gameBoard[e.target.id]) {
       if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[0] = 'x';
+        gameBoard.gameBoard[e.target.id] = 'x';
         gameBoard.playerTurn = 'o';
       } else {
-        gameBoard.gameBoard[0] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionTwo.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[1]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[1] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[1] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionThree.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[2]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[2] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[2] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionFour.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[3]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[3] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[3] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionFive.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[4]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[4] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[4] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionSix.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[5]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[5] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[5] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionSeven.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[6]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[6] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[6] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionEight.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[7]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[7] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[7] = 'o';
-        gameBoard.playerTurn = 'x';
-      }
-      display();
-      score();
-      ai();
-    }
-  });
-  gameBoard.positionNine.addEventListener('click', () => {
-    if (!gameBoard.gameBoard[8]) {
-      if (gameBoard.playerTurn === 'x') {
-        gameBoard.gameBoard[8] = 'x';
-        gameBoard.playerTurn = 'o';
-      } else {
-        gameBoard.gameBoard[8] = 'o';
+        gameBoard.gameBoard[e.target.id] = 'o';
         gameBoard.playerTurn = 'x';
       }
       display();
